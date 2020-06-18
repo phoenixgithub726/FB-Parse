237a7c7c331b7fe36ad72f62d9f88a3b2945c7bd
phoenixgithub726 <phoenix.devstar0726@gmail.com>
2020-06-18 17:43:44 +0800

    selenium custom page parse FIND US

--- scrapper/facebook.py
+++ scrapper/facebook.py
@@ -16,6 +16,7 @@ class FaceBook:
     def __init__(self):
         pass
 
+# face book login 
     def start(self):
         chrome.getInstance().doAction(message='Open facebook', action='open', params={'url': setting.getInstance().get('FACEBOOK')})
         chrome.getInstance().doAction(message='Waiting for loaded', action='wait_for_pageload')
@@ -28,5 +29,54 @@ class FaceBook:
         # html = chrome.getInstance().getHtml()
         # soup = Beautifulsoup(content)
 
-    def analysis(self):
-        pass
+    def parse(self):
+        with open("element/ruman.html", encoding="utf-8") as f:
+            data = f.read()
+            soup = BeautifulSoup(data, 'html.parser')
+            # div = soup.select('div[id^=PagesProfileAboutInfoPagelet_]')
+            div_profile = soup.select('div[id^= PagesProfileAboutInfoPagelet_]')
+            for div in div_profile:
+                # div_profile_children = div.findChildren('div', recursive=True)
+                div_profile_children = div.find_all('div', recursive=False)
+
+            # print("type:",type(div_profile_children))
+            
+            if len(div_profile_children) > 0:
+                # print("lensof profile",len(div_profile_children))
+                div_find_us = div_profile_children[1]
+                div_business_info = div_profile_children[2]
+            
+            mail_address = ""
+            mail_tag = soup.select('a[href^= mailto]')
+            if len(mail_tag) > 0:
+                for mail in mail_tag:
+                    mail_address += mail.text
+            mail_address = mail_address.strip()
+            print(mail_address)           
+    
+    def find_us(self, section_find_us):
+        # print(section_find_us)
+        div_find_us = section_find_us.findChildren('div',recursive=False)[0].findChildren('div',recursive=False)[0].findChildren('div', recursive=False)[0].findChildren('div', recursive=False)
+       
+        #FIND US div div_find_us[0]
+        div_title = div_find_us[0].find('span')
+        if div_title.text == "FIND US":
+            print("find us correnct")
+
+                
+        # Address Div div_find_us[1]
+        div_address = div_find_us[1].findChildren('div',recursive=False)
+        full_address = ""
+        for address in div_address[1].find_all('div'):
+            full_address += address.text + "  "
+            full_address = full_address.strip()
+            print(address.text)
+        # print("full-address", full_address)
+
+        # Phone Div div_find_us[3]
+        call_phone = ""
+        for phone in div_find_us[3].findChildren('div', recursive=False):
+            call_phone += phone.text
+            
+        call_phone = call_phone.strip()
+        print("stripeed", call_phone)
--- start.py
+++ start.py
@@ -3,6 +3,7 @@ from utils.log import log
 
 log.getInstance().printLog('Start the scrapper for instagram.')
 
-fb.getInstance().start()
+# fb.getInstance().start()
+fb.getInstance().parse()
 
 log.getInstance().printLog('End the scrapper for instagram. See you later.')
